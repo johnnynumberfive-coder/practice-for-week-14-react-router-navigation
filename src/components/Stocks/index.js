3de278522909cb652886f0c3f944418621677d74
John Nelson <135506733+johnnynumberfive-coder@users.noreply.github.com>
2025-04-16 12:54:28 +0000

    Adds navigation

--- src/components/Stocks/index.js
+++ src/components/Stocks/index.js
@@ -1,7 +1,19 @@
+import { Redirect, useHistory } from "react-router-dom";
+
 function Stocks() {
+  const history = useHistory();
+  const handleClick = () => {
+    window.alert("Sending info to DB!");
+    history.push("/");
+  };
+  const loggedIn = true;
+  if (!loggedIn) {
+    return <Redirect to="/not-logged-in"></Redirect>;
+  }
   return (
-    <div className='comp orange'>
+    <div className="comp orange">
       <h1>Stocks Component</h1>
+      <button onClick={handleClick}>Home</button>
     </div>
   );
 }
